eb112b49ff85be0b420755c5d222d285bc096d5d
Chris Sumners <chris.sumners@insightassured.com>
2016-01-24 19:08:39 +0000

    Initial setup and app name creation

---
+++ sumners.org.uk/sumners_org_uk.helper/Application.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumners_org_uk.helper
+{
+    public class App
+    {
+        public const string AppName = "Chris Sumners";
+        }
+    }
